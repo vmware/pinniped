3ea7403b6f38ed53af9dc67ce48b7188f306c98b
Ryan Richard <richardry@vmware.com>
2025-08-04 14:04:12 -0700

    update version and docs on website after release by creating a PR
    instead of doing a direct commit to the main branch

--- pipelines/shared-tasks/create-or-update-pr/task.sh
+++ pipelines/shared-tasks/create-or-update-pr/task.sh
@@ -5,7 +5,10 @@
 
 set -euo pipefail
 
-branch="${BRANCH:-"pinny/bump-deps"}"
+if [[ -z "${BRANCH:-}" || -z "${COMMIT_MESSAGE:-}" || -z "${PR_TITLE:-}" || -z "${PR_BODY:-}" ]]; then
+  echo "BRANCH, COMMIT_MESSAGE, PR_TITLE, and PR_BODY env vars are all required"
+  exit 1
+fi
 
 cd pinniped
 
@@ -33,9 +36,9 @@ fi
 
 # Check if the branch already exists on the remote.
 new_branch="no"
-if [[ -z "$(git ls-remote https_origin "$branch")" ]]; then
+if [[ -z "$(git ls-remote https_origin "$BRANCH")" ]]; then
   echo "The branch does not already exist, so create it."
-  git checkout -b "$branch"
+  git checkout -b "$BRANCH"
   git status
   new_branch="yes"
 else
@@ -46,7 +49,7 @@ else
   # Fetch all the remote branches so we can use one of them.
   git fetch https_origin
   # The branch already exists, so reuse it.
-  git checkout "$branch"
+  git checkout "$BRANCH"
   # Pull to sync up commits with the remote branch.
   git pull --rebase --autostash
   # Throw away all previous commits on the branch and set it up to look like main again.
@@ -64,14 +67,14 @@ git --no-pager diff --staged
 echo
 
 # Commit.
-echo "Committing changes to branch $branch. New branch? $new_branch."
-git commit -m "Bump dependencies"
+echo "Committing changes to branch $BRANCH. New branch? $new_branch."
+git commit -m "$COMMIT_MESSAGE"
 
 # Push.
 if [[ "$new_branch" == "yes" ]]; then
   # Push the new branch to the remote.
   echo "Pushing the new branch."
-  git push --set-upstream https_origin "$branch"
+  git push --set-upstream https_origin "$BRANCH"
 else
   # Force push the existing branch to the remote.
   echo "Force pushing the existing branch."
@@ -81,11 +84,10 @@ fi
 # Now check if there is already a PR open for our branch.
 # If there is already an open PR, then we just updated it by force pushing the branch.
 # Note that using the gh CLI without login depends on setting the GH_TOKEN env var.
-open_pr=$(gh pr list --head "$branch" --json title --jq '. | length')
+open_pr=$(gh pr list --head "$BRANCH" --json title --jq '. | length')
 if [[ "$open_pr" == "0" ]]; then
   # There is no currently open PR for this branch, so open a new PR for this branch
   # against main, and set the title and body.
   echo "Creating PR."
-  gh pr create --head "$branch" --base main \
-    --title "Bump dependencies" --body "Automatically bumped all go.mod direct dependencies and/or images in dockerfiles."
+  gh pr create --head "$BRANCH" --base main --title "$PR_TITLE" --body "$PR_BODY"
 fi
