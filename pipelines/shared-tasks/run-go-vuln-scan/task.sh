4bdfb816af5d564436b0f226c37ace81b48c4e7a
Ryan Richard <richardry@vmware.com>
2026-06-22 14:59:31 -0700

    stop using -json for govulncheck
    Signed-off-by: Ryan Richard <richardry@vmware.com>

--- pipelines/shared-tasks/run-go-vuln-scan/task.sh
+++ pipelines/shared-tasks/run-go-vuln-scan/task.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Copyright 2020-2024 the Pinniped contributors. All Rights Reserved.
+# Copyright 2020-2026 the Pinniped contributors. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
 set -euo pipefail
@@ -10,7 +10,7 @@ go install golang.org/x/vuln/cmd/govulncheck@latest
 
 cd pinniped
 
-OPTS="-json -test"
+OPTS="-test"
 
 if [[ -n "${BUILD_TAGS:-}" ]]; then
   OPTS="${OPTS} -tags ${BUILD_TAGS}"
